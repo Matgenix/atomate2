71e2ecf597b36a79a614dea38cfe3e39e550f2a5
Matthew Evans <git@ml-evs.science>
2022-11-22 17:28:16 +0000

    Beef up `run_lammps` and `LammpsSettings`

--- src/emdemate2/lammps/run.py
+++ src/emdemate2/lammps/run.py
@@ -1,9 +1,36 @@
-from pydantic import BaseSettings, Field
 import subprocess
+
+from pydantic import BaseSettings, Field
+
 
 class LammpsSettings(BaseSettings):
 
     LAMMPS_CMD: str = Field("lmp", description="The command to run LAMMPS.")
+    LAMMPS_SUFFIX: list[str] | str | None = Field(
+        None,
+        description=(
+            "The LAMMPS style suffix(es) to use."
+            "See https://docs.lammps.org/Run_options.html#suffix for more information."
+        ),
+        examples=["gpu", "kk", "intel", "omp", "opt", [["gpu", "kk"]]],
+    )
+    LAMMPS_PACKAGES: list[str] | str | None = Field(
+        None,
+        description=(
+            "Options to pass to the package command-line flag that controls subpackage "
+            "styles and parameters, e.g., `'gpu 1'` will call `lmp -pk gpu 1` will tell "
+            "LAMMPS to use 1 GPU for this calculation. "
+            "List values are passed with separate '-pk' invocations, e.g., `lmp -pk gpu 1 -pk omp 4`."
+            "See https://docs.lammps.org/Run_options.html#package for more information."
+        ),
+        examples=["gpu 0", "gpu 1 split 0.75", "gpu 2 split -1.0", "gpu 1 omp 4"],
+    )
+    MPI_CMD: str = Field("mpirun", description="The command to invoke MPI.")
+    MPI_NUM_PROCESSES_FLAG: str = Field(
+        "-n",
+        description="The flag with which to provide the number of processes to use in the MPI execution.",
+    )
+
 
 LAMMPS_SETTINGS = LammpsSettings()
 
@@ -11,21 +38,77 @@ LAMMPS_SETTINGS = LammpsSettings()
 def run_lammps(
     lammps_input_file: str,
     lammps_cmd: str = LAMMPS_SETTINGS.LAMMPS_CMD,
-    mpi_cmd: str = "mpirun",
-    omp_num_threads: int = 1,
+    lammps_suffix: list[str] | str | None = LAMMPS_SETTINGS.LAMMPS_SUFFIX,
+    lammps_pks: list[str] | str | None = LAMMPS_SETTINGS.LAMMPS_PACKAGES,
+    lammps_run_flags: list[str] | str | None = None,
+    mpi_cmd: str | None = LAMMPS_SETTINGS.MPI_CMD,
     mpi_num_processes: int = 1,
-    max_walltime: int | None = None,
+    mpi_num_processes_flag: str = LAMMPS_SETTINGS.MPI_NUM_PROCESSES_FLAG,
+    max_walltime_hours: float | None = None,
 ) -> None:
-    stdout = open("stdout.log", "a") 
-    stderr = open("stderr.log", "a")
-    process = subprocess.Popen(
-        [f"OMP_NUM_THREADS={omp_num_threads}", mpi_cmd, "-n", mpi_num_processes, lammps_cmd, "-in", lammps_input_file], stdout=stdout, stderr=stderr
-    )
+    """Run LAMMPS.
 
-    try:
-        process.communicate(timeout=max_walltime)
-    except subprocess.TimeoutExpired:
-        process.terminate()
+    Parameters:
+        lammps_input_file: The path to the main input file to be passed to the
+            LAMMPS executable with the `-in` command-line option.
+        lammps_cmd: The name or path to the LAMMPS executable.
+        lammps_suffix: The suffix to use that applies style variants at runtime
+            (see `LammpsSettings.LAMMPS_SUFFIX`).
+        lammps_pks: The runtime packages and options to tell LAMMPS to use
+            (see `LammpsSettings.LAMMPS_PACKAGES`).
+        lammps_run_kwargs: Any additional arbitrary flags to invoke LAMMPS with.
+        mpi_cmd: The command to invoke MPI (e.g., `'mpirun'` or `'mpiexec'`).
+            If None, invoke the `lammps_cmd` in serial mode.
+        mpi_num_processes: The number of MPI processes to pass to `mpi_cmd` with
+            the `mpi_num_processes_flag`.
+        mpi_num_processes_flag: The command-line flag to use with MPI to pass the number of processes,
+            e.g., `-n/n` [default] or `np/-np`.
+        max_walltime_hours: The maximum walltime in hours to allow for the task. If provided, attempts will
+            be made to cleanly end the calculation after this amount of time. Note: if using with a queueing system,
+            this value should leave suffcient time for the clean-up of the calculation within the maximum walltime
+            allocated to the job by the queue.
 
-    return
+    """
 
+    lammps_invocation: list[str] = []
+
+    if mpi_cmd is not None:
+        lammps_invocation.extend(
+            [
+                mpi_cmd,
+                f"-{mpi_num_processes_flag.lstrip('-')}",
+                str(mpi_num_processes),
+            ]
+        )
+
+    if lammps_suffix is not None:
+        if isinstance(lammps_suffix, str):
+            lammps_suffix = [lammps_suffix]
+        for sf in lammps_suffix:
+            lammps_invocation += ["-sf", sf]
+
+    if lammps_pks is not None:
+        if isinstance(lammps_pks, str):
+            lammps_pks = [lammps_pks]
+        for pk in lammps_pks:
+            lammps_invocation += ["-pk", pk]
+
+    if lammps_run_flags is not None:
+        if isinstance(lammps_run_flags, str):
+            lammps_run_flags = [lammps_run_flags]
+        for flag in lammps_run_flags:
+            lammps_invocation += [flag]
+
+    lammps_invocation.extend([lammps_cmd, "-in", lammps_input_file])
+
+    with open("stdout.log", "a") as stdout, open("stderr.log", "a") as stderr:
+        process = subprocess.Popen(
+            lammps_invocation,
+            stdout=stdout,
+            stderr=stderr,
+        )
+
+        try:
+            process.communicate(timeout=max_walltime_hours)
+        except subprocess.TimeoutExpired:
+            process.terminate()
